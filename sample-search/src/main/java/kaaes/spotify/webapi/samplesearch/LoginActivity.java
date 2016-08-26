ac86fb3f3feba4e7849ac203b83959e408d58380
SIMPLYBOYS <ferrari828@gmail.com>
2016-08-26 12:09:58 +0800

    initial commit

---
+++ sample-search/src/main/java/kaaes/spotify/webapi/samplesearch/LoginActivity.java
@@ -0,0 +1,147 @@
+package kaaes.spotify.webapi.samplesearch;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginActivity extends Activity implements PlayerNotificationCallback, ConnectionStateCallback{
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String CLIENT_ID = "your client id";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String REDIRECT_URI = "your redirect uri";
+
+    private static final int REQUEST_CODE = 1337;
+
+    private com.spotify.sdk.android.player.Player mPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String token = CredentialsHandler.getToken(this);
+        if (token == null) {
+            setContentView(R.layout.activity_login);
+        } else {
+            startMainActivity(token);
+        }
+    }
+
+    public void onLoginButtonClicked(View view) {
+
+        final AuthenticationRequest request = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+                .setScopes(new String[]{"streaming"})
+                .setShowDialog(true)
+                .build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    logMessage("Got token: " + response.getAccessToken());
+                    CredentialsHandler.setToken(this, response.getAccessToken(), response.getExpiresIn(), TimeUnit.SECONDS);
+                    startMainActivity(response.getAccessToken());
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    logError("Auth error: " + response.getError());
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    logError("Auth result: " + response.getType());
+            }
+        }
+    }
+
+    private void startMainActivity(String token) {
+        Intent intent = MainActivity.createIntent(this);
+        intent.putExtra(MainActivity.EXTRA_TOKEN, token);
+        startActivity(intent);
+        finish();
+    }
+
+    private void logError(String msg) {
+        Toast.makeText(this, "Error: " + msg, Toast.LENGTH_SHORT).show();
+        Log.e(TAG, msg);
+    }
+
+    private void logMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, msg);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+        switch (eventType) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+        switch (errorType) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+}
